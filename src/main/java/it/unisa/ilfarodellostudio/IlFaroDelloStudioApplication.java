1403c8523aeb6d11a1000c492f649dcbd659c576
Salvatore <s.amoroso11@studenti.unisa.it>
2025-12-01 09:37:31 +0100

    Initial commit

---
+++ src/main/java/it/unisa/ilfarodellostudio/IlFaroDelloStudioApplication.java
@@ -0,0 +1,13 @@
+package it.unisa.ilfarodellostudio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IlFaroDelloStudioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IlFaroDelloStudioApplication.class, args);
+    }
+
+}
