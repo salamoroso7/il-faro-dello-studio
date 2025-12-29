9b3e2f42166dbd0694a4769941719c214b88d751
Salvatore <s.amoroso11@studenti.unisa.it>
2025-12-29 19:06:57 +0100

    Add DTOs Aggiunte classi DTO per Famiglia, Docente e Studente in /users/dto

---
+++ src/main/java/it/unisa/ilfarodellostudio/users/dto/DocenteDto.java
@@ -0,0 +1,58 @@
+package it.unisa.ilfarodellostudio.users.dto;
+
+public class DocenteDto {
+    private String nome;
+    private String cognome;
+    private String email;
+    private String username;
+    private String password;
+    private String materia;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCognome() {
+        return cognome;
+    }
+
+    public void setCognome(String cognome) {
+        this.cognome = cognome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getMateria() {
+        return materia;
+    }
+
+    public void setMateria(String materia) {
+        this.materia = materia;
+    }
+}
