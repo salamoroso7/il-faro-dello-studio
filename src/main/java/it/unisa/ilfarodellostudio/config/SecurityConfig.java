ada77db83d8131a2b1eca431589b4889de5210e5
Salvatore <s.amoroso11@studenti.unisa.it>
2025-12-29 18:47:26 +0100

    Add SecurityConfig Aggiunta la classe SecurityConfig nel package /config

---
+++ src/main/java/it/unisa/ilfarodellostudio/config/SecurityConfig.java
@@ -0,0 +1,47 @@
+package it.unisa.ilfarodellostudio.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth
+                        // Permettiamo l'accesso ai form di registrazione nel UsersController
+                        .requestMatchers("/login", "/register/**", "/css/**", "/js/**").permitAll()
+
+                        // La creazione dello studente deve essere accessibile solo alla famiglia loggata
+                        .requestMatchers("/users/studente/**").hasRole("FAMIGLIA")
+
+                        .anyRequest().authenticated()
+                )
+                .formLogin(form -> form
+                        .loginPage("/login")
+                        .defaultSuccessUrl("/home", true)
+                        .permitAll()
+                )
+                .logout(logout -> logout
+                        .logoutUrl("/logout")
+                        .logoutSuccessUrl("/login?logout") // Dove andare dopo il logout
+                        .invalidateHttpSession(true)
+                        .deleteCookies("JSESSIONID")
+                        .permitAll()
+                );
+
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
